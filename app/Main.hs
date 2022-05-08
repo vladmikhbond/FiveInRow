3fddf5e06f4ed8bede87ace4955d89623d3269ec
volodymyr bondariev <vladmikhbond@hotmail.com>
2022-05-08 20:14:16 +0300

    .

--- app/Main.hs
+++ app/Main.hs
@@ -2,21 +2,41 @@ module Main where
 
 import Lib
 import Data.Array.IO
+import  Control.Monad (when)
+import Text.Read
 
 main :: IO ()
 main = do
-  t <- newTable  
+  t <- newTable
   run t
-  
 
-run :: IOArray Int Char -> IO ()
+-------------------------------------------------------
+run :: Table -> IO ()
 run t = do
   drawTable t
-  putStrLn "q-quit >>" 
+  putStrLn "q-quit >>"
   s <- getLine
-  if 'q' `elem` s 
+  if 'q' `elem` s
     then return ()
     else do
-      let (r, c) = divMod (read s) 10
-      put t r c 'x'
-      run t
+      let mbx = readMaybe s :: Maybe Int
+      case mbx of
+        Nothing -> run t
+        Just x -> do
+          let posX = divMod x 10
+          empty <- isCellEmpty t posX
+          when empty $ do
+              put t posX 'x'
+              posO <- stepO t
+              put t posO 'o'
+          run t
+
+
+test_findSample = do
+  t <- newTable
+  puts t [22, 23, 24] 'x'
+  puts t [51, 42, 33, 24, 15] 'x'
+  drawTable t
+  lst <- getElems t
+  let bounds = findSample lst " xxxxx "
+  print bounds
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,29 +1,37 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use putStrLn" #-}
-{-# LANGUAGE TupleSections #-}
+{-# HLINT ignore "Eta reduce" #-}
 
 
-
-module Lib ( size, get, put, puts, newTable, drawTable ) where
+module Lib (
+  size, get, put, puts, newTable, drawTable, findSample, isCellEmpty, stepO,rndStepO, 
+  test, getPotentPoses,
+  Table, Pos, Matr, Sample ) where
 
 import Data.Array.IO
+    ( getElems, readArray, writeArray, MArray(newArray), IOArray )
 import Consul
-
 import Control.Monad (sequence_, mapM_)
+import Data.Foldable (Foldable(foldl'))
+import Data.Time.Clock ( getCurrentTime, UTCTime(utctDayTime) )
+import Data.Functor ((<$>))
+import Data.Maybe ( fromJust, isJust )
+import System.Random
 
 type Table = IOArray Int Char
 type Pos = (Int, Int)
-
+type Matr = [Char]
+type Sample = String
 size = 10
 
+get :: Table -> Pos -> IO Char
+get table (i, j) = readArray table (i * size + j)
 
-get :: Table -> Int -> Int -> IO Char
-get arr i j = readArray arr (i * size + j)
+put :: Table -> Pos -> Char -> IO ()
+put table (i, j) = writeArray table (i * size + j)
 
-put :: Table -> Int -> Int -> Char -> IO ()
-put arr i j = writeArray arr (i * size + j)
-
-puts arr ps v = mapM_ (\(r, c) -> put arr r c v) ps
+puts :: Table -> [Int] -> Char -> IO ()
+puts table ns v = mapM_ (\n -> put table (divMod n 10) v) ns
 
 newTable :: IO Table
 newTable = newArray (0, size^2 - 1) ' '
@@ -32,18 +40,18 @@ newTable = newArray (0, size^2 - 1) ' '
 
 drawTable :: Table -> IO ()
 drawTable table = do
-  putStr clrscr  
+  putStr clrscr
   cs <-  getElems table
   sequence_ [drawCell cs r c | r <- [0..size - 1], c <- [0..size - 1]]
   putStrLn norm
 
 drawCell :: [Char] -> Int -> Int -> IO ()
-drawCell cs row col 
+drawCell cs row col
   | c == '\n' = putStr $ rc row' col' ++ "\n"
-  | c == 'x'  = putStr $ rc row' (col'+1) ++ green  ++ [c,c] 
-  | c == 'o'  = putStr $ rc row' (col'+1) ++ yellow ++ [c,c] 
-  | otherwise = putStr $ rc row' (col'+1) ++ gray ++ show row ++ show col 
-  where 
+  | c == 'x'  = putStr $ rc row' (col'+1) ++ green  ++ [c,c]
+  | c == 'o'  = putStr $ rc row' (col'+1) ++ yellow ++ [c,c]
+  | otherwise = putStr $ rc row' (col'+1) ++ gray ++ show row ++ show col
+  where
     i = size * row + col
     c = cs !! i
     col' = (col + 1) * 3
@@ -56,47 +64,128 @@ insertSepByN n [] sep = []
 insertSepByN n xs sep = let (zs, rs) = splitAt n xs
                in zs ++ [sep] ++ insertSepByN n rs sep
 ---------------------------------------------------------
+
+--
+findSample :: Matr -> Sample -> [(Pos, Pos)]
+findSample matr xs = let
+  n = length xs
+  val r c = matr !! (r * size + c)
+
+  hor = [horEq r c | r <- [0..size-1], c <- [0..size-n] ]
+  ver = [verEq r c | r <- [0..size-n], c <- [0..size-1] ]
+  dia = [diaEq r c | r <- [0..size-n], c <- [0..size-n] ]
+  aid = [aidEq r c | r <- [0..size-n], c <- [n-1..size-1] ]
+
+  horEq r c = if [val r (c+i)     | i <- [0..n-1]] /= xs then Nothing
+    else Just ((r, c), (r, c+n-1))
+  verEq r c = if [val (r+i) c     | i <- [0..n-1]] /= xs then Nothing
+    else Just ((r, c), (r+n-1, c))
+  diaEq r c = if [val (r+i) (c+i) | i <- [0..n-1]] /= xs then Nothing
+    else Just ((r, c), (r+n-1, c+n-1))
+  aidEq r c = if [val (r+i) (c-i) | i <- [0..n-1]] /= xs then Nothing
+    else Just ((r, c), (r+n-1, c-n+1))
+ in
+  [ fromJust x |  x <- hor ++ ver ++ dia ++ aid, isJust x]
+
+
+findSamples :: Matr -> [Sample] -> [(Pos, Pos)]
+findSamples matr = concatMap (findSample matr)
+
+findSamples2 :: Matr -> [Sample] -> [(Pos, Pos, Sample)]
+findSamples2 matr samples = concatMap f samples 
+ where
+   f sample  = (\(p1, p2) -> (p1, p2, sample)) <$> findSample matr sample
+
+getPotentPos :: (Pos, Pos, Sample) -> [Pos]     -- getPotentPos ((1, 5), (4, 2), "x x ")  ->  [(2,4),(4,2)]
+getPotentPos ((r1, c1), (r2, c2), sample) = 
+  [(r, c) | (r, c, s) <- zip3 [r1..r2] [c1, c1+dCol..c2] sample, s == ' ']
+ where 
+  dCol = signum (c2 - c1)
+
+getPotentPoses :: [(Pos, Pos, Sample)] -> [Pos]     -- getPotentPoses [((1, 5), (4, 2), "x x ")]  ->  [(2,4),(4,2)]
+getPotentPoses ts = concatMap getPotentPos ts
+
+
+isCellEmpty :: Table -> Pos -> IO Bool
+isCellEmpty table (r, c) = do
+  if r >= 0 && r < size && c >= 0 && c < size
+    then do
+      v <- get table (r, c)
+      return $ v == ' '
+    else
+      return False
+-------------------------------------------------------------
+
+-- tbl = do
+--   t <- newTable
+--   puts t [(2,2), (2,3), (2,4)] 'x'
+--   --puts t [(2,2), (3,2), (4,2)] 'x'
+--   --puts t [(1,1), (2,2), (3,3)] 'x'
+--   puts t [(5,1), (4,2), (3,3), (2,4), (1,5)] 'x'
+
+--   pos <- stepO t
+--   return ()
+samples = [
+  " oooo", "o ooo", "oo oo", "ooo o", "oooo ", 
+  " xxxx", "x xxx", "xx xx", "xxx x", "xxxx ", 
+  " ooo", "ooo ", " xxx", "xxx ", " oo", "oo " , " xx", "xx ", " o", "o " 
+  ]
+
+stepO :: Table -> IO Pos
+stepO t = do
+  drawTable t
+  lst <- getElems t
+  let bounds = findSamples lst samples
+  --putStr "bounds: " >> print bounds -------------------
+  let ps = concat [ [p1, p2] | (p1, p2) <- bounds]
+  empties <- filterEmptyPlaces t ps
+  --putStr "empties: " >> print empties -------------------
+  if null empties
+    then rndStepO t
+    else (return . head) empties
+  
+rndStepO :: Table -> IO Pos 
+rndStepO t = do
+   g0 <- initStdGen
+   let (a, g1) = uniformR (0, 9) g0 :: (Int, StdGen) 
+   let (b, g) = uniformR (0, 9) g1 :: (Int, StdGen) 
+   empty <- isCellEmpty t (a, b) 
+   if empty 
+     then return (a, b)  
+     else rndStepO t
+
 test = do
   t <- newTable
-  
-  --puts t [(2,2), (2,3), (2,4)] 'x'
-  --puts t [(2,2), (3,2), (4,2)] 'x'
-  --puts t [(1,1), (2,2), (3,3)] 'x'
-  puts t [(5,1), (4,2), (3,3), (2,4), (1,5)] 'x'
+  puts t [0..90] 'x'
   drawTable t
-  lst <- getElems t 
-  let bounds = findNx 5 'x' lst
-  print bounds
- 
+  p <- rndStepO t
+  print p
 
---findNx :: Int -> Char -> [Char] -> Maybe (Pos, Pos) 
-findNx :: Eq p => Int -> p -> [p] -> Maybe ((Int, Int), (Int, Int))
-findNx n char tab = let 
-   hor = [(r, c) | r <- [0..size-1], c <- [0..size-n] ]
-   ver = [(r, c) | r <- [0..size-n], c <- [0..size-1] ]
-   dia = [(r, c) | r <- [0..size-n], c <- [0..size-n] ]
-   aid = [(r, c) | r <- [0..size-n], c <- [n-1..size-1] ]
-   
-   tst (r, c) = tab !! (r * size + c) == char
 
-   tstH (r, c) = all (\x -> tst (r, c+x))   [0..n-1]
-   tstV (r, c) = all (\x -> tst (r+x, c))   [0..n-1]
-   tstD (r, c) = all (\x -> tst (r+x, c+x)) [0..n-1]
-   tstA (r, c) = all (\x -> tst (r+x, c-x)) [0..n-1]
- 
-   hor' = dropWhile (not . tstH) hor
-   ver' = dropWhile (not . tstV) ver
-   dia' = dropWhile (not . tstD) dia
-   aid' = dropWhile (not . tstA) aid
 
- in 
-  if not (null hor')
-  then let (a, b) = head hor' in Just ((a, b), (a, b + n - 1))
-  else if not (null ver')
-  then let (a, b) = head ver' in Just ((a, b), (a + n - 1, b))
-  else if not (null dia')
-  then let (a, b) = head dia' in Just ((a, b), (a + n - 1, b + n - 1))
-  else if not (null aid')
-  then let (a, b) = head aid' in Just ((a, b), (a + n - 1, b - n + 1))
-  else Nothing
+
+filterEmptyPlaces :: Table -> [Pos] -> IO [Pos]
+filterEmptyPlaces t [] = return []
+filterEmptyPlaces t (p: ps) = do
+  b <- isCellEmpty t p
+  ps' <- filterEmptyPlaces t ps
+  if b 
+    then return (p : ps')
+    else return ps'
+
+
+
+-- stepO :: Table -> IO Pos
+-- stepO table = do
+--   rnd <- rnd100
+--   let pos = rnd `divMod` 10
+--   isEmpty <- isCellEmpty table pos
+--   if isEmpty
+--     then return pos 
+--     else stepO table
+{-- 
+  знаходимо межі всіх зразків, впорядковані по зменшості ціни - findSamples
+  серед знайдених меж обираємо першу, яка припадає на вільне поле
+  ставимо в неї "о"                              
+--}
 
